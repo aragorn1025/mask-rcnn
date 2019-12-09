8487ab914f60632374fb3dc708965571f84f54b4
Jia-Rou <miny0702.eo07g@nctu.edu.tw>
2019-12-09 16:32:32 +0800

    merge to LabelMeDataset

--- utilities/datasets/LabelMeDataset.py
+++ utilities/datasets/LabelMeDataset.py
@@ -1,14 +1,167 @@
+import base64
+import io
+import numpy as np
+import PIL.Image
+import math
+
+def img_b64_to_arr(img_b64):
+    f = io.BytesIO()
+    f.write(base64.b64decode(img_b64))
+    img_arr = np.array(PIL.Image.open(f))
+    return img_arr
+
+def shape_to_mask(img_shape, points, shape_type=None,
+                  line_width=10, point_size=5):
+    mask = np.zeros(img_shape[:2], dtype=np.uint8)
+    mask = PIL.Image.fromarray(mask)
+    draw = PIL.ImageDraw.Draw(mask)
+    xy = [tuple(point) for point in points]
+    if shape_type == 'circle':
+        assert len(xy) == 2, 'Shape of shape_type=circle must have 2 points'
+        (cx, cy), (px, py) = xy
+        d = math.sqrt((cx - px) ** 2 + (cy - py) ** 2)
+        draw.ellipse([cx - d, cy - d, cx + d, cy + d], outline=1, fill=1)
+    elif shape_type == 'rectangle':
+        assert len(xy) == 2, 'Shape of shape_type=rectangle must have 2 points'
+        draw.rectangle(xy, outline=1, fill=1)
+    elif shape_type == 'line':
+        assert len(xy) == 2, 'Shape of shape_type=line must have 2 points'
+        draw.line(xy=xy, fill=1, width=line_width)
+    elif shape_type == 'linestrip':
+        draw.line(xy=xy, fill=1, width=line_width)
+    elif shape_type == 'point':
+        assert len(xy) == 1, 'Shape of shape_type=point must have 1 points'
+        cx, cy = xy[0]
+        r = point_size
+        draw.ellipse([cx - r, cy - r, cx + r, cy + r], outline=1, fill=1)
+    else:
+        assert len(xy) > 2, 'Polygon must have points more than 2'
+        draw.polygon(xy=xy, outline=1, fill=1)
+    mask = np.array(mask, dtype=bool)
+    return mask
+
+def shapes_to_label(img_shape, shapes, label_name_to_value, type='class'):
+    assert type in ['class', 'instance']
+
+    cls = np.zeros(img_shape[:2], dtype=np.int32)
+    if type == 'instance':
+        ins = np.zeros(img_shape[:2], dtype=np.int32)
+        instance_names = ['_background_']
+    for shape in shapes:
+        points = shape['points']
+        label = shape['label']
+        shape_type = shape.get('shape_type', None)
+        if type == 'class':
+            cls_name = label
+        elif type == 'instance':
+            cls_name = label.split('-')[0]
+            if label not in instance_names:
+                instance_names.append(label)
+            ins_id = instance_names.index(label)
+        cls_id = label_name_to_value[cls_name]
+        mask = shape_to_mask(img_shape[:2], points, shape_type)
+        cls[mask] = cls_id
+        if type == 'instance':
+            ins[mask] = ins_id
+
+    if type == 'instance':
+        return cls, ins
+    return cls
+
+def labelme_shapes_to_label(img_shape, shapes):
+
+    label_name_to_value = {'_background_': 0}
+    for shape in shapes:
+        label_name = shape['label']
+        if label_name in label_name_to_value:
+            label_value = label_name_to_value[label_name]
+        else:
+            label_value = len(label_name_to_value)
+            label_name_to_value[label_name] = label_value
+
+    lbl = shapes_to_label(img_shape, shapes, label_name_to_value)
+    return lbl, label_name_to_value
+################################################################################
+
+import os
 import torch
 import torch.utils.data
+import PIL
+from torchvision import transforms
+
+import json
+import PIL.Image
 
 class LabelMeDataset(torch.utils.data.Dataset):
-    def __init__(self):
-		super(LabelMeDataset, self).__init__()
-        self._data = []
-        raise NotImplementedError('LabelMeDataset.__init__() is not implement yet.')
+    def __init__(self, root, resize, cropsize):
+        self._root = root
+        self._resize = resize
+        self._cropsize = cropsize
 
-    def __len__(self):
-        return len(self._data)
+        self._json = list(sorted(os.listdir(os.path.join(root))))
 
     def __getitem__(self, index):
-        return torch.tensor(self._data[index][0]), torch.tensor(self._data[index][1])
+        json_path = os.path.join(self._root, self._json[index])
+        if os.path.isfile(json_path):
+            data = json.load(open(json_path))
+            img = img_b64_to_arr(data['imageData'])
+            lbl, lbl_names = labelme_shapes_to_label(img.shape, data['shapes'])
+            img = PIL.Image.fromarray(img)
+            mask = PIL.Image.fromarray(lbl)
+            labels=[]
+            for lbl_name in lbl_names:
+                labels.append(lbl_name)        
+
+        transform = transforms.Compose([transforms.Resize(self._resize),
+                                        transforms.CenterCrop(self._cropsize),
+                                        transforms.ToTensor(),
+                                        ])
+        img = transform(img)
+
+        transforms_target = transforms.Compose([transforms.Resize(self._resize, PIL.Image.NEAREST),
+                                                transforms.CenterCrop(self._cropsize),
+                                                ])
+        mask = transforms_target(mask)
+        mask = np.array(mask)
+        
+        obj_ids = np.unique(mask)
+        obj_ids = obj_ids[1:]
+
+        masks = mask == obj_ids[:, None, None]
+
+        num_objs = len(obj_ids)
+        boxes = []
+        for i in range(num_objs):
+            pos = np.where(masks[i])            
+            xmin = np.min(pos[1])
+            xmax = np.max(pos[1])
+            ymin = np.min(pos[0])
+            ymax = np.max(pos[0])
+            boxes.append([xmin, ymin, xmax, ymax])
+
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        labels = labels[1:]
+        labels = list(map(int, labels))
+        labels = np.array(labels)
+        labels = torch.tensor(labels, dtype=torch.int64)
+
+        masks = torch.as_tensor(masks, dtype=torch.uint8)
+
+        image_id = torch.tensor([index])
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+
+        iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
+
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+        target["masks"] = masks
+        target["image_id"] = image_id
+        target["area"] = area
+        target["iscrowd"] = iscrowd
+                    
+
+        return img, target
+
+    def __len__(self):
+        return len(self._json)
